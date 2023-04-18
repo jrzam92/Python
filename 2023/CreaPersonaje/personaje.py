93c9b2408fa964648a1a7ccec4521733917ab850
jrzam92 <jr_zam92@outlook.com>
2023-04-18 03:40:39 -0600

    Se realiza un POC de creacion de personajes mostrando al ultimo lo que el usuario ingreso

--- 2023/CreaPersonaje/personaje.py
+++ 2023/CreaPersonaje/personaje.py
@@ -1,43 +1,69 @@
-v_titulo="crea tu personaje"
-#Demographics
-v_nombre=intput("Escribe el nombre de tu perosnaje a continuacion: ")
-v_edad=0
-v_genero=""
-v_etinia=""
-v_ocupacion=""
-v_educacion=""
-v_estatus_socio_economico=False
-#Physical Appearance
-v_altura=0.00
-v_peso=0.00
-v_color_piel=""
-v_color_pelo=""
-v_tamaño_pelo=""
-v_forma_cara=""
-v_forma_cejas=""
-v_forma_pestañas=""
-v_numero_ojos=0
-v_forma_ojos=""
-v_color_ojos=""
-v_numero_orejas=0
-v_forma_orejas=""
-v_has_bigote=False
-v_has_barba=False
-v_tipo_cuerpo=""
-v_complexion=""
-v_tatuajes=""
-v_has_birthmarks=False
-v_discapaciidades=""
-v_accesorios=""
-v_debilidades=""
-#Bio
-v_birthdate="01/01/0001"
-v_place_birth
-
-
-
-v_apariencia="apariencia"
-v_adorns_equals="==============="
-
-
-print(v_apariencia.upper())
+titulo = "CREACION DE PERSONAJES"
+tituloFinal = "ESTE ES EL RESULTADO DE TU PERSONAJE"
+#Preguntas al usuario
+_Nombre = "Cual es el nombre?  "
+_Edad = "Cual es la edad?  "
+_Estatura = "Cual es la estatura?  "
+_Peso = "Cual es el peso?  " 
+_Sexo = "Cual es el genero?  "
+_Contextura="Cual es la contextura?  "
+_ColorOjos="Cual es el color de ojos?  "
+_ColorCabello="Cual es el color de cabello?  "
+_ColorPiel="Cual es el color de Piel?  "
+_Raza="Cual es la Raza?  "
+_DescripcionGeneral="Describe un brevemente su biografia?  "
+_Health="Cual seria el rango de salud?, especifica de 0 a 100  "
+_Power= "Cual seria el rango de poder?, especifica de 0 a 100  "
+_Energy= "Cual seria el rango de energia?, especifica de 0 a 100  "
+#Extras
+saltoLinea = "\n"
+print(titulo)
+print("====================================")
+print("====================================")
+_Nombre=(input(_Nombre))
+print("====================================") 
+print("====================================")
+_Edad=(input(_Edad))
+print("====================================") 
+print("====================================")
+_Estatura=(input(_Estatura))
+print("====================================") 
+print("====================================")
+_Peso=(input(_Peso))
+print("====================================") 
+print("====================================")
+_Sexo=(input(_Sexo))
+print("====================================") 
+print("====================================")
+_Contextura=(input(_Contextura))
+print("====================================") 
+print("====================================")
+_ColorOjos=(input(_ColorOjos))
+print("====================================") 
+print("====================================")
+_ColorCabello=(input(_ColorCabello))
+print("====================================") 
+print("====================================")
+_ColorPiel=(input(_ColorPiel))
+print("====================================") 
+print("====================================")
+_Raza=(input(_Raza))
+print("====================================") 
+print("====================================")
+_DescripcionGeneral=(input(_DescripcionGeneral))
+print("====================================") 
+print("====================================")
+_Health=(input(_Health))
+print("====================================") 
+print("====================================")
+_Power=(input(_Power))
+print("====================================") 
+print("====================================")
+_Energy=(input(_Energy))
+Resultado =f"{saltoLinea} El nombre de tu personaje es :  {_Nombre }{saltoLinea} La edad que tiene es de :  {_Edad} {saltoLinea} Mide : {_Estatura}  {saltoLinea} El peso es de : {_Peso} {saltoLinea} Su genero es :  {_Sexo}{saltoLinea} Su contextura es :  {_Contextura}  {saltoLinea} Su color de ojos es :  {_ColorOjos} {saltoLinea} Su color de cabello es :  {_ColorCabello}  {saltoLinea} Su color de piel es: {_ColorPiel}  {saltoLinea} Su Raza es :  {_Raza}  {saltoLinea} Biografia : {saltoLinea}  {_DescripcionGeneral}  {saltoLinea} Salud :{_Health}  {saltoLinea} Poder :  {_Power} {saltoLinea} Enrgia : {_Energy}"
+print("====================================") 
+print("====================================")
+print(tituloFinal)
+print("====================================")
+print(Resultado)
+print("====================================")
